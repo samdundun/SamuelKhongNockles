6fb4ea495d0b2278a14b1ae143c30a289544c450
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-11-13 10:37:29 -0500

    asdfasdf

---
+++ src/javaTheHardWay/Trait.java
@@ -0,0 +1,48 @@
+package javaTheHardWay;
+
+public class Trait {
+
+	public static final String[] traits = {"striped", "long-tailed","furry","brown","short-eared","spotted","short-tailed","hairless","white","long-earted"};
+	private String description;
+	private int index;
+	
+	public static void main(String[] args) {
+		Trait longTailed = new Trait(1);
+		Trait shortTailed = new Trait(6);
+		System.out.println(longTailed + " and " + shortTailed + " are the same trait: "+ longTailed.equals(shortTailed));
+
+	}
+	
+	public Trait() {
+		index = (int)(Math.random()*traits.length);
+		description = traits[index];
+	}
+	
+	public Trait(int index) {
+		this.index = index;
+		description = traits[index];
+	}
+	
+	public String toString() {
+		return description;
+	}
+
+	private int getIndex() {
+		return index;
+	}
+	
+	public boolean equals(Object o) {
+		return (o instanceof Trait) && (this.getIndex()-((Trait)o).getIndex())%(traits.length/2)==0;
+	}
+	
+	public static Trait getDominantTrait(Trait t1, Trait t2) {
+		if(t1.equals(t2) && t1.getIndex() < t2.getIndex())return t1;
+		else if(t1.equals(t2) && t2.getIndex() < t1.getIndex())return t2;
+		else if(Math.random() < .5) return t1;
+		return t2;
+	}
+	
+	//1. If they are instances and if they appear in the same pair in the traits array
+	//2. If it appears earlier in the traits array it is dominant
+	//3. One is for creating the object while the other one is set automatically to them.
+}
